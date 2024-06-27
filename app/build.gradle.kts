f8aa57ee586123c9f85a49dd9c57a1276179c26f
THAIDTN\Admin <thaidtn.ssquad@gmail.com>
2024-06-27 17:15:10 +0700

    release library v101

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.snake.squad.adslibrary"
         minSdk = 28
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 101
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
